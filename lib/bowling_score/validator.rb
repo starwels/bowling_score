746740ab68bb31d336486cd71ad6249a1f0fa831
wellingtonx <wlss.lucas@gmail.com>
2020-12-03 14:45:32 -0300

    create validations

---
+++ lib/bowling_score/validator.rb
@@ -0,0 +1,36 @@
+module BowlingScore
+  class InvalidValue < StandardError
+    def initialize(msg = "Values must be between 0 and 10")
+      super
+    end
+  end
+
+  class InvalidThrowsLength < StandardError
+    def initialize(msg = "Rolls must be between 20 and 21")
+      super
+    end
+  end
+
+  class Validator
+    def initialize(values:)
+      @values = values
+    end
+
+    def call
+      validate_values
+      validate_rolls
+    end
+
+    private
+
+    def validate_values
+      @values.each do |value|
+        raise InvalidValue if !value.between?(0, 10)
+      end
+    end
+
+    def validate_rolls
+      raise InvalidThrowsLength if !@values.size.between?(20,21)
+    end
+  end
+end
---
+++ test/validator_test.rb
@@ -0,0 +1,16 @@
+require 'test/unit'
+
+require_relative '../lib/bowling_score/validator'
+
+class ValidatorTest < Test::Unit::TestCase
+  include BowlingScore
+
+  def test_invalid_input
+    assert_raise( InvalidValue ) { Validator.new(values: [1, 2, 3, -1]).call }
+    assert_raise( InvalidThrowsLength ) { Validator.new(values: (0..10).to_a).call }
+  end
+
+  def test_valid_input
+    assert_nothing_raised { Validator.new(values: Array.new(21, 1) ).call }
+  end
+end
